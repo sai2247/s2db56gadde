426b894dff60f8586e703292729009249136710b
sai2247 <98117189+sai2247@users.noreply.github.com>
2022-04-13 21:57:57 -0500

    upd

--- app.js
+++ app.js
@@ -4,17 +4,61 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+  console.log("Connection to DB succeeded")}); 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dollargeneralRouter = require('./routes/dollargeneral');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var dollargeneral = require("./models/dollargeneral"); 
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+  // Delete everything
+  await dollargeneral.deleteMany();
+ 
+  let instance1 = new dollargeneral({Itemname:"Coca-cola", Quantity:23, price:"Tweleve USD"});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+ 
+  let instance2 = new dollargeneral({Itemname:"Pepsi", Quantity:40, price:"Twenty USD"});
+  instance2.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("Second object saved")
+  });
+ 
+  let instance3 = new dollargeneral({Itemname:"Seven-up", Quantity:28, price:"Thirty USD"});
+  instance3.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("Third object saved")
+  });
+ 
+ }
+
+ let reseed = true; 
+ if (reseed) { recreateDB();}
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ models/dollargeneral.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose") 
+const dollargeneralSchema = mongoose.Schema({ 
+    Itemname: String, 
+    Quantity: Number, 
+    price: String 
+}) 
+ 
+module.exports = mongoose.model("dollargeneral", 
+dollargeneralSchema)
