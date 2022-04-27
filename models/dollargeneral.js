434ea0b8b6dd0b162e5b837c8c2ce28e735c1e9f
sai2247 <98117189+sai2247@users.noreply.github.com>
2022-04-27 12:43:08 -0500

    updating

--- models/dollargeneral.js
+++ models/dollargeneral.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose") 
 const dollargeneralSchema = mongoose.Schema({ 
     Itemname: String, 
-    Quantity: Number, 
-    price: String 
-}) 
+    Quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: String,
+         min: 2,
+         max: 5000
+    }
+    })
  
 module.exports = mongoose.model("dollargeneral", dollargeneralSchema)
--- routes/dollargeneral.js
+++ routes/dollargeneral.js
@@ -1,21 +1,36 @@
 var express = require('express');
-const dollargeneral_controlers= require('../controllers/dollargeneral'); 
+const dollargeneral_controller = require('../controllers/dollargeneral');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login. 
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET home page. */
-//router.get('/', function(req, res, next) {
- // res.render('dollargeneral', { title: 'search result' });
-//});
-router.get('/', dollargeneral_controlers.dollargeneral_view_all_Page );
-/* GET detail dollargeneral page */ 
-router.get('/detail', dollargeneral_controlers.dollargeneral_view_one_Page); 
-/* GET create dollargeneral page */ 
-router.get('/create', dollargeneral_controlers.dollargeneral_create_Page);
+/*
+router.get('/', function(req, res, next) {
+  res.render('dollargenerals', { title: 'Search Results' });
+});
+*/
+/* GET detail costume page */
+router.get('/detail', dollargeneral_controller.dollargeneral_view_one_Page);
 
-/* GET create update page */ 
-router.get('/update', dollargeneral_controlers.dollargeneral_update_Page); 
+/* GET create costume page */
+router.get('/create',secured, dollargeneral_controller.dollargeneral_create_Page);
 
-/* GET delete dollargeneral page */ 
-router.get('/delete', dollargeneral_controlers.dollargeneral_delete_Page); 
+/* GET costumes */
+router.get('/', dollargeneral_controller.dollargeneral_view_all_Page);
+
+/* GET create update page */
+router.get('/update',secured, dollargeneral_controller.dollargeneral_update_Page);
+
+/* GET delete costume page */
+router.get('/delete',secured,dollargeneral_controller.dollargeneral_delete_Page);
 
 module.exports = router;
